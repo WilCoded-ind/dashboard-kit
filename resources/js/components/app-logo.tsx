4f39cad26ef3bec33be0e8def2192abe2fc7b858
Wildan-AhmadF <codedbywil@gmail.com>
2026-04-15 02:23:18 +0700

    refactor(sidebar) : ubah logo

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -1,14 +1,14 @@
-import AppLogoIcon from '@/components/app-logo-icon';
-
 export default function AppLogo() {
     return (
         <>
-            <div className="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground">
-                <AppLogoIcon className="size-5 fill-current text-white dark:text-black" />
+            <div className="flex aspect-square size-8 items-center justify-center md:size-10">
+                <img src="/images/logo.png" alt="FahleviLabs" />
             </div>
             <div className="ml-1 grid flex-1 text-left text-sm">
-                <span className="mb-0.5 truncate leading-tight font-semibold">
-                    Laravel Starter Kit
+                <span className="pt-1 truncate leading-tight font-semibold">
+                    <span className="text-primary">Fahlevi&nbsp;  
+                    </span>
+                    Dashboard Kit
                 </span>
             </div>
         </>
